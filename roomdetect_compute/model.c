a4f9a5b6bb38df7bd591b962a432632f6bca4640
Jeremiah Griffin <jgrif007@ucr.edu>
2017-06-05 22:13:43 -0700

    Correct the heurustic for classification
    The closest room is the room with the smallest absolute z-score,
    not the largest.

--- roomdetect_compute/model.c
+++ roomdetect_compute/model.c
@@ -22,13 +22,13 @@ void update_vector(vector_t *v, accum x)
 {
     size_t n = v->samples + 1;
     accum delta = x - v->mean;
-    accum mean = v->mean + delta / n;
+    accum mean = v->mean + delta / (accum)n;
     accum var = v->var + delta * (x - mean);
 
     v->samples = n;
     v->mean = mean;
     v->var = var;
-    v->stdev = sqrt(var / n);
+    v->stdev = (accum)sqrt((double)(var / (accum)n));
 }
 
 accum score_vector(const vector_t *v, accum x)
@@ -52,6 +52,7 @@ void update_room(room_t *r, const accum *x)
     for (size_t i = 0; i < MODEL_VECTOR_SIZE; ++i) {
         update_vector(&r->vectors[i], x[i]);
     }
+    ++r->samples;
 }
 
 accum score_room(const room_t *r, const accum *x)
@@ -61,7 +62,7 @@ accum score_room(const room_t *r, const accum *x)
     }
     accum z = 0K;
     for (size_t i = 0; i < MODEL_VECTOR_SIZE; ++i) {
-        z += score_vector(&r->vectors[i], x[i]);
+        z += absk(score_vector(&r->vectors[i], x[i]));
     }
     return z;
 }
@@ -75,18 +76,18 @@ void clear_model(model_t *m)
 
 size_t classify_model(const model_t *m, const accum *x)
 {
-    size_t i_max = 0xFF;
-    accum z_max = 0K;
+    size_t i_close = 0xFF;
+    accum z_close = ACCUM_MAX;
     for (size_t i = 0; i < MODEL_ROOMS_MAX; ++i) {
         if (m->rooms[i].samples == 0) {
             continue;
         }
 
         accum z = score_room(&m->rooms[i], x);
-        if (z > z_max) {
-            i_max = i;
-            z_max = z;
+        if (z < z_close) {
+            i_close = i;
+            z_close = z;
         }
     }
-    return i_max;
+    return i_close;
 }
